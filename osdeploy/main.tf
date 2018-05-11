de57dfbfb3ff96abe5fae58fa06d556ff5effc20
Darryl <darfrost@za.ibm.com>
2018-05-11 17:34:36 +0000

    Rebuilt OpenStack template

--- osdeploy/main.tf
+++ osdeploy/main.tf
@@ -4,13 +4,40 @@
 ##
 #####################################################################
 
+## REFERENCE {"network1":{"type": "openstack_reference_network"}}
+
 terraform {
   required_version = "> 0.8.0"
 }
 
-provider "OpenStack" {
+
+provider "openstack" {
+  user_name   = "${var.openstack_user_name}"
+  password    = "${var.openstack_password}"
+  tenant_name = "${var.openstack_project_name}"
+  domain_name = "${var.openstack_domain_name}"
+  auth_url    = "${var.openstack_auth_url}"
+  region      = "${var.openstack_region}"
   insecure    = true
   version = "~> 1.2"
 }
 
+resource "openstack_compute_instance_v2" "LinuxVM" {
+  name      = "${var.LinuxVM_name}"
+  image_name  = "${var.openstack_image_name}"
+  flavor_name = "${var.openstack_flavor_name}"
+  key_pair  = "${openstack_compute_keypair_v2.auth.id}"
+  network {
+    uuid = "${var.openstack_network1_id}"
+  }
+}
+
+resource "tls_private_key" "ssh" {
+    algorithm = "RSA"
+}
+
+resource "openstack_compute_keypair_v2" "auth" {
+    name = "openstack-temp-public-key"
+    public_key = "${tls_private_key.ssh.public_key_openssh}"
+}
 
--- osdeploy/variables.tf
+++ osdeploy/variables.tf
@@ -4,3 +4,53 @@
 ##
 #####################################################################
 
+variable "openstack_user_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_password" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_project_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_domain_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_auth_url" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_region" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "LinuxVM_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_image_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_flavor_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_network1_id" {
+  type = "string"
+  description = "Generated"
+}
+
