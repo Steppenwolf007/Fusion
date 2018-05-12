22c588b7f1c6a7cfbb59e973a3fbeed9a994d777
darrylf08 <darfrost@za.ibm.com>
2018-05-12 17:50:31 +0800

    Update main.tf

--- osdeploy/main.tf
+++ osdeploy/main.tf
@@ -25,7 +25,11 @@ resource "openstack_compute_instance_v2" "LinuxVM" {
   image_name  = "${var.openstack_image_name}"
   flavor_name = "${var.openstack_flavor_name}"
   key_pair  = "${var.openstack_keypair_name}"
+  network {
+    name = "Public_Service" 
+    }
+  availability_zone = "manage-az"
+  security_groups = {"67854e08-d7ba-42f0-b253-5b81d92c6a2b"]
 }
-
 #
 
